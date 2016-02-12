6cccec2257186b0b01e30487f8c6c739aa64fc4b
Daniele Sluijters <daenney@users.noreply.github.com>
2016-02-12 10:17:13 +0100

    conf: Fix setting of environment variables

--- conf.d/rbenv.fish
+++ conf.d/rbenv.fish
@@ -1,5 +1,5 @@
 set -l rbenv_root (rbenv root)
-set -U fish_user_paths $fish_user_paths $rbenv_root/shims
-set -xg RBENV_SHELL fish
+set -x PATH $rbenv_root/shims $PATH
+set -x RBENV_SHELL fish
 command mkdir -p $rbenv_root/{shims,versions}
 command rbenv rehash 2>/dev/null

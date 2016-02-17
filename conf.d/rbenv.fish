ed1d16d774f6d7fa289df6db90140f33fb677207
Daniele Sluijters <daenney@users.noreply.github.com>
2016-02-17 23:34:38 +0100

    conf.d: Only create directories if necessary
    By checking for the existence of either directory we can be relatively
    sure that both exist or not. If one don't we just fire the `mkdir` for
    both as that won't cost us anything extra.

--- conf.d/rbenv.fish
+++ conf.d/rbenv.fish
@@ -1,4 +1,6 @@
 set -l rbenv_root (rbenv root)
 set -x PATH $rbenv_root/shims $PATH
 set -x RBENV_SHELL fish
-command mkdir -p $rbenv_root/{shims,versions}
+if test ! -d "$rbenv_root/shims"; or test ! -d "$rbenv_root/versions"
+    command mkdir -p $rbenv_root/{shims,versions}
+end

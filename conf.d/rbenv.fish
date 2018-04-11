08a988669f2a3aecd102df493bf3adf20e7c0feb
Bjorn Neergaard <bjorn@neersighted.com>
2018-04-11 04:40:19 -0600

    Update rbenv.fish
    Update to match the pyenv plugin; most importantly, add a `--is-login` guard.

--- conf.d/rbenv.fish
+++ conf.d/rbenv.fish
@@ -1,18 +1,20 @@
 if not command -s rbenv > /dev/null
-    echo "rbenv: command not found. See https://github.com/rbenv/rbenv"
+    echo "Install <github.com/rbenv/rbenv> to use 'rbenv'."
     exit 1
 end
 
-set -l rbenv_root ''
+set -l rbenv_root ""
+
 if test -z "$RBENV_ROOT"
-    set rbenv_root "$HOME/.rbenv"
-    set -x RBENV_ROOT "$HOME/.rbenv"
+    set rbenv_root ~/.rbenv
+    set -x RBENV_ROOT "$rbenv_root"
 else
     set rbenv_root "$RBENV_ROOT"
 end
 
-set -x PATH $rbenv_root/shims $PATH
-set -x RBENV_SHELL fish
-if test ! -d "$rbenv_root/shims"; or test ! -d "$rbenv_root/versions"
-    command mkdir -p $rbenv_root/{shims,versions}
+if status --is-login
+    set -x PATH "$rbenv_root/shims" $PATH
+    set -x RBENV_SHELL fish
 end
+command mkdir -p "$rbenv_root/"{shims,versions}
+
